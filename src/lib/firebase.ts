ff632dd965579397b74dcc42449b5f2b4635055e
Bobby Moghi <big.smokev69@gmail.com>
2025-07-11 16:47:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -6,13 +5,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAJStYL8kkv7xfQ3LvMsxUwdLWyOIFW1Vk",
+  authDomain: "zerocycle-b1a93.firebaseapp.com",
+  projectId: "zerocycle-b1a93",
+  storageBucket: "zerocycle-b1a93.appspot.com",
+  messagingSenderId: "955055643388",
+  appId: "1:955055643388:web:321c3f59759e89df1e1579",
+  measurementId: "G-MY099RZZ25",
 };
 
 // Initialize Firebase
